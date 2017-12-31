6eef95abcd5b4e83f434e063efc45ea0c09c1e40
skirk@LISD <skirk@lubbockisd.org>
2017-12-31 16:58:16 -0600

    My code
    Replaced boilerplate with my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## These functions manage the caching of the inverse of a matrix. 
+## 
 
-## Write a short comment describing this function
-
+##
+## Creates a list of functions that handle the caching.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+##
+## Returns the inverse matrix if it is found in the cache. 
+## If not found, it creates the inverse, caches and returns it.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
